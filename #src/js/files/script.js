da42ef0329d43c57aa4c4d9a80e69a97697a8b53
V Zhevaha <v.zhevaga@gmail.com>
2021-10-17 04:17:41 -0400

    work with link hover in header

--- #src/js/files/script.js
+++ #src/js/files/script.js
@@ -1,20 +1,20 @@
 // подсказка при наведениии =======================================
 // (отрабатывает только при перезагрузке страницы) ================
-		function showTippy(){
-			let element = document.querySelector('._icon-phone');
-			let windowWeight = window.innerWidth;
-			// console.log(windowWeight);
-			if ( windowWeight <= 1152){
-				// console.log("add _call-back");
-				element.classList.add('_call-back');
-			}else{
-				// console.log("remove _call-back");
-				element.classList.remove('_call-back');
-			}
-		};
+		// function showTippy(){
+		// 	let element = document.querySelector('._icon-phone');
+		// 	let windowWeight = window.innerWidth;
+		// 	// console.log(windowWeight);
+		// 	if ( windowWeight <= 1152){
+		// 		// console.log("add _call-back");
+		// 		element.classList.add('_call-back');
+		// 	}else{
+		// 		// console.log("remove _call-back");
+		// 		element.classList.remove('_call-back');
+		// 	}
+		// };
 		
-		showTippy();
-		tippy('._call-back', {
-			content:"Заказать обратный звонок",
-	});
+	// 	showTippy();
+	// 	tippy('._call-back', {
+	// 		content:"Заказать обратный звонок",
+	// });
 // ================================================================
